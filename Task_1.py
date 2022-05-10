6a3cd8f28d40bc5ab8f0fff1eecf46255b0e2c4f
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-10 16:36:54 +0300

    Overridden methods __sub__, __add__, __mull__, __truediv__

--- Task_1.py
+++ Task_1.py
@@ -20,21 +20,57 @@ class Fraction:
 
     # instantiation
     def __init__(self, num: int, den: int) -> None:
-        self.__num = num    # numerator
-        self.__den = den    # denominator
+        self.num = num    # numerator
+        self.den = den    # denominator
 
     # subtraction
-    def __sub__(self, other) -> None:
-        pass
+    def __sub__(self, other_sub):
+        # if type(other_sub) == int:
+        if type(other_sub) == type(self):
+            if self.den == other_sub.den:
+                new_num = self.num * other_sub.den - self.den * other_sub.num
+                new_den = self.den * other_sub.den
+                return Fraction(new_num, new_den)
+            else:
+                new_num = self.num - other_sub.num
+                new_den = self.den
+                return Fraction(new_num, new_den)
+        raise TypeError('Must be int')
 
     # addition
-    def __add__(self, other) -> None:
-        pass
+    def __add__(self, other_add):
+        if type(other_add) == type(self):
+            if self.den == other_add.den:
+                new_num = self.num*other_add.den + self.den*other_add.num
+                new_den = self.den * other_add.den
+                return Fraction(new_num, new_den)
+            else:
+                new_num = self.num + other_add.num
+                new_den = self.den
+                return Fraction(new_num, new_den)
+        raise TypeError('Must be int')
 
     # multiplication
-    def __mul__(self, other) -> None:
-        pass
+    def __mul__(self, other_mul):
+        return Fraction(self.num * other_mul.num, self.den * other_mul.den)
 
     # division
-    def __truediv__(self, other) -> None:
-        pass
+    def __truediv__(self, other_div):
+        return Fraction(self.num / other_div.num, self.den / other_div.den)
+
+
+p1 = Fraction(2, 2)
+p2 = Fraction(2, 5)
+
+sub = p1 - p2
+add = p1 + p2
+mul = p1 + p2
+div = p1 / p2
+
+
+print(p1)
+print(p2)
+print(sub)
+print(add)
+print(mul)
+print(div)
