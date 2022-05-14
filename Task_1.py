0c2b8cb91ff313931c098edb039cbfd0248a6f96
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-14 22:35:48 +0300

    Added class FractionMixin and staticmethods

--- Task_1.py
+++ Task_1.py
@@ -23,9 +23,22 @@ class Fraction:
         self.__num = num  # numerator
         self.__den = den  # denominator
 
+    def set_den(self):
+        pass
+
+    @property
+    def num(self):
+        return self.__num
+        pass
+
+    @property
+    def den(self):
+        return self.__den
+        pass
+
     # subtraction
     def __sub__(self, other_sub):
-        if self.__num == other_sub.__num:       # TODO не выполняется проверка равенства знаменателей
+        if self.__den != other_sub.__den:
             new_num = self.__num * other_sub.__den - self.__den * other_sub.__num
             new_den = self.__den * other_sub.__den
             return Fraction(new_num, new_den)
@@ -36,11 +49,11 @@ class Fraction:
 
     # addition
     def __add__(self, other_add):
-        if self.__den == other_add.__den:    # TODO не выполняется проверка равенства знаменателей
+        if self.__den != other_add.__den:
             new_num = self.__num * other_add.__den + self.__den * other_add.__num
             new_den = self.__den * other_add.__den
             return Fraction(new_num, new_den)
-        if self.__den != other_add.__den:
+        else:
             new_num = self.__num + other_add.__num
             new_den = self.__den
             return Fraction(new_num, new_den)
@@ -55,22 +68,54 @@ class Fraction:
 
     def __str__(self) -> str:
         return f"{self.__num}/{self.__den}"
-        # return str(self.__num) + "/" + str(self.__den)
+
+    @classmethod
+    def ret_fraction(cls, __num, __den):
+        return cls(__num, __den)
+        pass
 
 
-p1 = Fraction(1, 2)
+class FractionMixin:
 
-p2 = Fraction(2, 2)
+    @staticmethod
+    def add(fr1, fr2):
+        if fr1.den != fr2.den:
+            newnum = fr1.num * fr2.den + fr1.den * fr2.num
+            newden = fr1.den * fr2.den
+            return Fraction(newnum, newden)
+        else:
+            newnum = fr1.num + fr2.num
+            newden = fr1.den
+            return Fraction(newnum, newden)
 
-sub = p1 - p2
-add = p1 + p2
-mul = p1 * p2
-div = p1 / p2
+    @staticmethod
+    def sub(fr1, fr2):              #TODO finalize the sub
+        return Fraction()
+        pass
 
-print("\nFirst fraction:", p1)
-print("Second fraction:", p2, "\n")
+    @staticmethod
+    def mul(fr1, fr2):              #TODO finalize the mul
+        return Fraction()
+        pass
+
+    @staticmethod
+    def div(fr1, fr2):              #TODO finalize the div
+        return Fraction()
+        pass
+
+fr1 = Fraction(1, 2)
+
+fr2 = Fraction(1, 3)
+
+sub = fr1 - fr2
+add = fr1 + fr2
+mul = fr1 * fr2
+div = fr1 / fr2
+
+print("\nFirst fraction:", fr1)
+print("Second fraction:", fr2, "\n")
 print("Result of subtracting fractions:", sub, "\n")
-print("Result of adding fractions:", add, "\n")
-print("Result of multiplying fractions:", mul, "\n")
-print("Result of dividing fractions:", div)
-# print()
+# print("Result of adding fractions:", add, "\n")
+# print("Result of multiplying fractions:", mul, "\n")
+# print("Result of dividing fractions:", div)
+print("Result adding fractions (mixin):", FractionMixin.add(fr1, fr2))
