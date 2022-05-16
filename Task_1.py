c33e005ad2d2bc103700f5f3d0df64e7cde535e0
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-16 12:04:02 +0300

    All function work

--- Task_1.py
+++ Task_1.py
@@ -16,96 +16,114 @@
 """
 
 
-class Fraction:
+class FractionMixin:
+    """Multipying fractions and returns a fraction"""
+
+    @staticmethod
+    def add(fraction_1, fraction_2):
+        """Adds fractions and returns a fraction"""
+        if fraction_1.den != fraction_2.den:
+            num_add = fraction_1.num * fraction_2.den + fraction_1.den * fraction_2.num
+            den_add = fraction_1.den * fraction_2.den
+            return Fraction(num_add, den_add)
+        else:
+            num_add = fraction_1.num + fraction_2.num
+            den_add = fraction_1.den
+            return Fraction(num_add, den_add)
+
+    @staticmethod
+    def sub(fraction_1, fraction_2):
+        """Subtracts fractions and returns a fraction"""
+        if fraction_1.den != fraction_2.den:
+            num_sub = fraction_1.num * fraction_2.den - fraction_1.den * fraction_2.num
+            den_sub = fraction_1.den * fraction_2.den
+            return Fraction(num_sub, den_sub)
+        else:
+            num_sub = fraction_1.num - fraction_2.num
+            den_sub = fraction_1.den
+            return Fraction(num_sub, den_sub)
+
+    @staticmethod
+    def mul(fraction_1, fraction_2):
+        """Multiplying fractions and returns a fraction"""
+        return Fraction(fraction_1.num * fraction_2.num, fraction_1.den * fraction_2.den)
+
+    @staticmethod
+    def div(fraction_1, fraction_2):
+        """Divides fractions and returns a fraction"""
+        return Fraction(fraction_1.num * fraction_2.den, fraction_1.den * fraction_2.num)
+
+
+class Fraction(FractionMixin):
 
     # instantiation
     def __init__(self, num: int, den: int) -> None:
         self.__num = num  # numerator
-        self.__den = den  # denominator
-
-    def set_den(self):
-        pass
+        self.den = den  # denominator
 
     @property
-    def num(self):
+    def num(self) -> int:
+        """Return int"""
         return self.__num
-        pass
 
     @property
-    def den(self):
+    def den(self) -> int:
+        """Return int"""
         return self.__den
-        pass
+
+    @den.setter
+    def den(self, den) -> None:
+        if den == 0:
+            raise Exception("Denominator can't be a zero")
+        self.__den = den
 
     # subtraction
     def __sub__(self, other_sub):
-        if self.__den != other_sub.__den:
-            new_num = self.__num * other_sub.__den - self.__den * other_sub.__num
-            new_den = self.__den * other_sub.__den
+        """Subtracts fractions and returns a fraction"""
+        if self.den != other_sub.den:
+            new_num = self.__num * other_sub.den - self.den * other_sub.__num
+            new_den = self.den * other_sub.den
             return Fraction(new_num, new_den)
         else:
             new_num = self.__num - other_sub.__num
-            new_den = self.__den
+            new_den = self.den
             return Fraction(new_num, new_den)
 
     # addition
     def __add__(self, other_add):
-        if self.__den != other_add.__den:
-            new_num = self.__num * other_add.__den + self.__den * other_add.__num
-            new_den = self.__den * other_add.__den
+        """Adds fractions and returns a fraction"""
+        if self.den != other_add.den:
+            new_num = self.__num * other_add.den + self.den * other_add.__num
+            new_den = self.den * other_add.den
             return Fraction(new_num, new_den)
         else:
             new_num = self.__num + other_add.__num
-            new_den = self.__den
+            new_den = self.den
             return Fraction(new_num, new_den)
 
     # multiplication
     def __mul__(self, other_mul):
-        return Fraction(self.__num * other_mul.__num, self.__den * other_mul.__den)
+        """Multiplays fractions and returns a fraction"""
+        return Fraction(self.__num * other_mul.__num, self.den * other_mul.den)
 
     # division
     def __truediv__(self, other_div):
-        return Fraction(self.__num * other_div.__den, self.__den * other_div.__num)
+        """Dividing fractions and returns a fraction"""
+        return Fraction(self.__num * other_div.den, self.den * other_div.__num)
 
     def __str__(self) -> str:
-        return f"{self.__num}/{self.__den}"
+        return f"{self.__num}/{self.den}"
 
     @classmethod
-    def ret_fraction(cls, __num, __den):
-        return cls(__num, __den)
-        pass
+    def change_str(cls, str_fraction):
+        """Change string to fraction"""
+        num, den = map(int, str_fraction.split('/'))
+        return cls(num, den)
 
-
-class FractionMixin:
-
-    @staticmethod
-    def add(fr1, fr2):
-        if fr1.den != fr2.den:
-            newnum = fr1.num * fr2.den + fr1.den * fr2.num
-            newden = fr1.den * fr2.den
-            return Fraction(newnum, newden)
-        else:
-            newnum = fr1.num + fr2.num
-            newden = fr1.den
-            return Fraction(newnum, newden)
-
-    @staticmethod
-    def sub(fr1, fr2):              #TODO finalize the sub
-        return Fraction()
-        pass
-
-    @staticmethod
-    def mul(fr1, fr2):              #TODO finalize the mul
-        return Fraction()
-        pass
-
-    @staticmethod
-    def div(fr1, fr2):              #TODO finalize the div
-        return Fraction()
-        pass
 
 fr1 = Fraction(1, 2)
 
-fr2 = Fraction(1, 3)
+fr2 = Fraction.change_str('2/3')
 
 sub = fr1 - fr2
 add = fr1 + fr2
@@ -113,9 +131,12 @@ mul = fr1 * fr2
 div = fr1 / fr2
 
 print("\nFirst fraction:", fr1)
-print("Second fraction:", fr2, "\n")
-print("Result of subtracting fractions:", sub, "\n")
-# print("Result of adding fractions:", add, "\n")
-# print("Result of multiplying fractions:", mul, "\n")
-# print("Result of dividing fractions:", div)
-print("Result adding fractions (mixin):", FractionMixin.add(fr1, fr2))
+print("Second fraction:", fr2)
+print("Result of subtracting fractions:", sub)
+print("Result of adding fractions:", add)
+print("Result of multiplying fractions:", mul)
+print("Result of dividing fractions:", div)
+print("Result subtracting fractions (mixin):", Fraction.sub(fr1, fr2))
+print("Result adding fractions (mixin):", Fraction.add(fr1, fr2))
+print("Result multiplying fractions (mixin):", FractionMixin.mul(fr1, fr2))
+print("Result dividing fractions (mixin):", FractionMixin.div(fr1, fr2))
