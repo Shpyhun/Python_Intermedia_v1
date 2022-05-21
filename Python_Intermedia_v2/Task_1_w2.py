ee64cc2854eb577401954dc6cc9b638a6d0bcee3
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-21 18:06:19 +0300

    Task_2 weak 2 created class calculator. Сonverted first and third input value to float type.The second value was checked for +, -, *, /.

--- Python_Intermedia_v2/Task_1_w2.py
+++ Python_Intermedia_v2/Task_1_w2.py
@@ -1,4 +1,3 @@
-
 """
 1) Необходимо написать функцию калькулятор, которая принимает строку состоящую из числа, оператора и второго числа
 разделенных пробелом. Например ('1 + 1'); Необходимо разделить строку используя str.split(), и проверить является
@@ -16,3 +15,23 @@ c) Если второе значение ввода не является '+', 
 """
 
 
+def calculator(str_calc: str):
+    if len(str_calc.split()) != 3:
+        return "FormulaError"
+    num_1, oper, num_2 = str_calc.split()
+    try:
+        num_1 = float(num_1)
+        num_2 = float(num_2)
+    except ValueError:
+        return "FormulaError"
+    if oper not in "+-*/":
+        return "FormulaError"
+
+    # if num_2 == None:
+    return num_1, oper, num_2
+
+
+
+calc = "11 + 22"
+
+print(calculator(calc))
