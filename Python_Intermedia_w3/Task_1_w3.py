cb29eab654073286991968e9bdca0bb17a30769e
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-28 13:45:19 +0300

    Task_1_w3.py weak 3 created function read_file

--- Python_Intermedia_w3/Task_1_w3.py
+++ Python_Intermedia_w3/Task_1_w3.py
@@ -7,17 +7,16 @@
 
 def read_file():
     try:
-        # file = open(input("Input name file: "), encoding='utf-8')
-        file = open('1.txt', encoding='utf-8')
-        new_file = open(input("Input name new file: "), 'w', encoding='utf-8')
-        file.readline()
-    except TypeError:
-        raise "Error name file"
-    # except FileNotFoundError:
-    #     raise "Error name file"
-    finally:
-        file.close()
-        new_file.close()
+        with open(input("Input name file: "), encoding='utf-8') as f:
+            with open(input("Input name new file: "), 'a', encoding='utf-8') as nf:
+                count = 0
+                for row in f:
+                    count += 1
+                    nf.write(f'{count}: {row}')
+    except FileNotFoundError:
+        print('Error name file')
+    except IOError:
+        print('Something else')
 
 
 read_file()
