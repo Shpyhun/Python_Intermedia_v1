d4cae46158e56a9dc0d76107e93473a8f0fbc30b
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-05-28 18:09:58 +0300

    Task_1_w3.py weak 3 add 3 functions sentence_without_punctuation, sentence_without_punctuation, longest_word

---
+++ Python_Intermedia_w3/word_utils.py
@@ -0,0 +1,19 @@
+def sentence_without_punctuation(sentence):
+    """Returns a sentence without punctuation"""
+    new_sign = ''
+    for sign in sentence:
+        if sign not in r"!#$%&\'\"()*+,-./:;<=>?@[\\]^_`{|}~":
+            new_sign += sign
+    return new_sign
+
+
+def sentence_without_punctuation(sentence):
+    """Returns a list of words from a sentence"""
+    words_list = [sign for sign in sentence.split()]
+    return words_list
+
+
+def longest_word(sentence):
+    """Returns the longest word in a sentence"""
+    words_list = [sign for sign in sentence.split()]
+    return max(words_list)
